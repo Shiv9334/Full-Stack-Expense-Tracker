920d6eaf51e60ea1161ed726cdb716fe8ecd38b6
Shiv <shivkumar9334@gmail.com>
2023-03-25 10:49:29 +0530

    task 19

--- model/expense.js
+++ model/expense.js
@@ -1,23 +1,25 @@
 const Sequelize = require("sequelize");
 const sequelize = require("../util/database");
 
-const Expense = sequelize.define("expense", {
+const User = sequelize.define("userexpense", {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true,
   },
-  amount: { type: Sequelize.INTEGER, allowNull: false },
+  amount: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
   description: {
     type: Sequelize.STRING,
     allowNull: false,
-    distinct: true,
   },
   category: {
     type: Sequelize.STRING,
     allowNull: false,
-    distinct: true,
   },
 });
-module.exports = Expense;
+
+module.exports = User;
--- util/database.js
+++ util/database.js
@@ -1,4 +1,5 @@
 const Sequelize = require("sequelize");
+
 const sequelize = new Sequelize("book-appointment", "root", "9122892311", {
   dialect: "mysql",
   host: "localhost",
